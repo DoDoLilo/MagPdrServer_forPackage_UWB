1.The mag position server which has been test in the Beijing environment, found several important bugs and fixed it. 2.Bug 1: do not put the calculation of the ENTRANCE_LIST in the "for loop". 3.Bug 2: the "new_xy" should align to the tail of "time_arr" while using the "add_head_to_xy()" function. 4.Socket server improvement: the network environment in the Beijing is so special that app-socket-client can connect() succeed when my socket server is closed. So I added the response "MMPS\n" when server accept() a app-socket-client-connect.

build_save_mag_map_by_robot.py
@@ -116,7 +116,7 @@ if __name__ == '__main__':
                     sync_data = SRI.sync_robot_pos_and_imu_data(gt_imu_offset_arr[line_index][0],
                                                                 gt_imu_offset_arr[line_index][1],
                                                                 float(gt_imu_offset_arr[line_index][2]))
-                    for row in sync_data:
+                    for row in sync_data[5:len(sync_data)]:
                         gt_imu_sync_data.append(row)
             gt_imu_sync_data = np.array(gt_imu_sync_data, dtype=float)
 

mag_map_build/sync_robot_and_imu_data.py
@@ -46,17 +46,17 @@ def read_robot_result(robot_txt_file):
 def sync_robot_pos_and_imu_data(robot_txt_file, imu_csv_file, offset):
     # 调用read_robot_result()获取[ms, robot_x, robot_y]
     robot_data_list = read_robot_result(robot_txt_file)
-    imu_data_list = np.loadtxt(imu_csv_file, delimiter=',')
+    imu_data_arr = np.loadtxt(imu_csv_file, delimiter=',')
 
     # 机器人位置频率更低，将手机imu数据合并到机器人那边，以保证文件不会过大!
     imu_index = 0
-    imu_len = len(imu_data_list)
+    imu_len = len(imu_data_arr)
     sync_data_list = []
 
     for robot_line in robot_data_list:
         imu_time = robot_line[0] + offset
         # 在imu数据中找到对应时间
-        while imu_index < imu_len and float(imu_data_list[imu_index][0]) < imu_time:
+        while imu_index < imu_len and float(imu_data_arr[imu_index][0]) < imu_time:
             imu_index += 1
 
         if imu_index < imu_len:
@@ -64,7 +64,7 @@ def sync_robot_pos_and_imu_data(robot_txt_file, imu_csv_file, offset):
             temp_data = []
             for d1 in robot_line:
                 temp_data.append(d1)
-            for d2 in imu_data_list[imu_index][1:]:
+            for d2 in imu_data_arr[imu_index][1:]:
                 temp_data.append(float(d2))
             sync_data_list.append(temp_data)
         else:
@@ -73,15 +73,16 @@ def sync_robot_pos_and_imu_data(robot_txt_file, imu_csv_file, offset):
     return sync_data_list
 
 
-# robot_test_txt = "D:/pythonProjects/MagPdrServer_forPackage/test/robot_data_test/robot_test_data.txt"
+# robot_test_txt = "D:\pythonProjects\MagPdrServer_forPackage\\test\\robot_data_test\\robot_result.txt"
 # imu_test_csv = "D:/pythonProjects/MagPdrServer_forPackage/test/robot_data_test/imu_fake_test_data.csv"
-#
+
 # robot_data = read_robot_result(robot_test_txt)
 # print("robot data:")
 # print(len(robot_data), ',', len(robot_data[0]))
+# np.savetxt("D:\pythonProjects\MagPdrServer_forPackage\\test\\robot_data_test\\robot_xy.csv", robot_data, delimiter=',')
 # for d in robot_data:
 #     print(d)
-#
+
 # sync_data = sync_robot_pos_and_imu_data(robot_test_txt, imu_test_csv, 0)
 # print(len(sync_data), ',', len(sync_data[0]))
 # print(len(sync_data[0]))

server_threads/mag_position_thread.py
@@ -52,6 +52,12 @@ class MagPositionThread(threading.Thread):
         self.MOVE_X = configurations.MoveX
         self.MOVE_Y = configurations.MoveY
 
+        # 先要将entrance_list坐标根据coordinate_offset映射到0-map_size_x, 0-map_size_y的坐标系下 → move_x, move_y
+        # 注意提前在这里做了，而不是在mag_position_thread里每次都做，导致重复平移entrance_list、第一次后都错了
+        for e in range(0, len(self.ENTRANCE_LIST)):
+            self.ENTRANCE_LIST[e][0] += self.COORDINATE_OFFSET[0]
+            self.ENTRANCE_LIST[e][1] += self.COORDINATE_OFFSET[1]
+
         mag_map = MMT.rebuild_map_from_mvh_files(self.MAG_MAP_FILES)
         if mag_map is None:
             print("地磁指纹文件错误，初始化失败！")
@@ -59,13 +65,12 @@ class MagPositionThread(threading.Thread):
         self.mag_map = mag_map
 
     def run(self) -> None:
-        self.mag_position_thread(self.in_data_queue, self.out_data_queue, self.COORDINATE_OFFSET, self.ENTRANCE_LIST)
+        self.mag_position_thread(self.in_data_queue, self.out_data_queue, self.ENTRANCE_LIST)
 
-    # 输入：容器引用、地图坐标系参数（左下角、右上角坐标）、地图所有入口的坐标
-    #    coordinate_offset，将entrance坐标平移到指纹库坐标系0-map_size_x, 0-map_size_y的move_x,move_y
+    # 输入：容器引用、地图坐标系参数（左下角、右上角坐标）、地图所有被平移到指纹库坐标系的入口坐标
     # 从in_data_queue中获取pdr_thread输出的 [time, [pdr_x, y], [10*[mag x, y, z]]]
     # 往out_data_list中放入[time, [mag_position_x,y]]
-    def mag_position_thread(self, in_data_queue, out_data_queue, coordinate_offset, entrance_list) -> None:
+    def mag_position_thread(self, in_data_queue, out_data_queue, entrance_list) -> None:
         transfer = None
         first_window_start_distance = self.INITAIL_BUFFER_DIS + self.SLIDE_BLOCK_SIZE * self.SLIDE_STEP - self.BUFFER_DIS  # 第一个滑动窗口的起始距离
         window_buffer = []
@@ -74,6 +79,7 @@ class MagPositionThread(threading.Thread):
         slide_distance = self.SLIDE_STEP * self.SLIDE_BLOCK_SIZE
 
         while True:
+            print("state is:", self.state)
             # 根据不同的状态对该数据做对应的处理
             if self.state == MagPositionState.STOP:
                 # 重新开始
@@ -91,11 +97,13 @@ class MagPositionThread(threading.Thread):
                 window_start = False
                 distance = 0
 
-                # 从数据输入流中取地足够初始遍历的数据。注意如果过程中遇到-1，则回到STOP状态
+                # 从数据输入流中取地足够初始遍历的数据。注意如果过程中遇到END，则回到STOP状态
                 while distance < self.INITAIL_BUFFER_DIS:
                     cur_data = in_data_queue.get()
                     if isinstance(cur_data, str) and cur_data == 'END':
                         self.state = MagPositionState.STOP
+                        print("END recived.")
+                        # print("state is:", self.state)
                         break
 
                     last_data = inital_data_buffer[len(inital_data_buffer) - 1]
@@ -112,10 +120,6 @@ class MagPositionThread(threading.Thread):
 
                 if self.state == MagPositionState.STOP:
                     continue
-                # 先要将entrance_list坐标根据coordinate_offset映射到0-map_size_x, 0-map_size_y的坐标系下 → move_x, move_y
-                for e in range(0, len(entrance_list)):
-                    entrance_list[e][0] += coordinate_offset[0]
-                    entrance_list[e][1] += coordinate_offset[1]
 
                 # 初始化搜索成功，状态转移至稳定搜索阶段，*而pdr坐标，并不需要使用move xy!都会包含在transfer里面
                 # 预处理pdr发送过来的数据，将[N][time, [pdr_x, y], [10*[mag x, y, z, quat x, y, z, w]], pdr_index]变为
@@ -134,8 +138,9 @@ class MagPositionThread(threading.Thread):
 
                 if inital_transfer is None:
                     # 怎么办？认为失败了
-                    print("初始搜索失败，请检查使用规范！")
+                    print("初始搜索失败，请确保指纹库建立正确、覆盖面积足够，or 减少初始窗口大小（但要保证比滑动窗口大）。")
                     self.state = MagPositionState.STOP
+                    # print("state is:", self.state)
                     continue
 
                 # 将结果放入输出队列中，注意外部如果不及时取走结果，这一步可能（队列满）会阻塞，导致后续代码全部停止！
@@ -147,8 +152,8 @@ class MagPositionThread(threading.Thread):
                 # 记录结果
                 position_save_arr = self.add_head_to_xy(inital_map_xy, time_arr)
                 # self.print_mag_position_msg(self.add_head_to_xy(inital_map_xy, time_arr))
-                # print("Initial Transfer = ", transfer)
-                # print("Initial Loss = ", inital_loss)
+                print("Initial Transfer = ", transfer)
+                print("Initial Loss = ", inital_loss)
                 continue
 
             if self.state == MagPositionState.STABLE_RUNNING:
@@ -170,7 +175,7 @@ class MagPositionThread(threading.Thread):
                         # out_data_queue.put('END')
                         # out_data_queue.put(np.array(pdr_index_list))
                         # self.write_data_to_sock_file('END')
-                        # print('END')
+                        print('END')
                         break
 
                     last_data = window_buffer[len(window_buffer) - 1]
@@ -206,7 +211,7 @@ class MagPositionThread(threading.Thread):
                         # out_data_queue.put(new_xy)
                         # self.write_data_to_sock_file(self.add_head_to_xy(new_xy))
                         # 记录地磁定位结果
-                        position_save_arr = np.vstack((position_save_arr, self.add_head_to_xy(new_xy, time_arr)))
+                        position_save_arr = np.vstack((position_save_arr, self.add_head_to_xy(new_xy, time_arr[len(time_arr)-len(new_xy):len(time_arr)])))
                         # self.print_mag_position_msg(self.add_head_to_xy(new_xy, time_arr))
                         # print("transfer = ", transfer)
                         # print("window points number = ", len(window_buffer))